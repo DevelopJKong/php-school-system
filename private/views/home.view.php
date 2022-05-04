dd7e84be8d8663baa0209478e451d1014a556419
DevelopJKong <ybaram40@gmail.com>
2022-05-04 10:20:06 +0900

    #08 Adding css files 완료

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -3,8 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/all.min.css">
+    <!--<script src="https://kit.fontawesome.com/520a1f1065.js" crossorigin="anonymous"></script>-->
 </head>
 <body>
-    <h1>This is home</h1>
+<div class="container-fluid">
+        <h1><i class="fa fa-plus"></i>This is home</h1>
+    </div>
 </body>
 </html>
